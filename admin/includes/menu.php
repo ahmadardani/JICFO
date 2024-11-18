4658c8449f166e83f38b5860a501cb8479d2ff2d
Ahmad Ardani <59734602+akasak4@users.noreply.github.com>
2024-11-19 07:45:00 +0800

    add more images and changed the icons

--- admin/includes/menu.php
+++ admin/includes/menu.php
@@ -15,8 +15,6 @@
             integrity=
     "sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
             crossorigin="anonymous"> 
-     
-        
     </head>
     
     <body>
